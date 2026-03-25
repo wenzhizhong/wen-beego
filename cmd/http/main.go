2db0a22cdb74f1604963f85c252c18af0eb55f8e
qing <zhizhong_wen@163.com>
2026-03-25 15:46:28 +0800

    grace: beego grace hot release

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"WenBeego/apps/common/global"
 	cmdCommon "WenBeego/cmd/common"
 	_ "WenBeego/routers"
-	"fmt"
+	"os"
 	"strconv"
 
 	_ "github.com/beego/beego/v2/core/config/yaml"
 	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/grace"
 )
 
 func main() {
@@ -19,8 +21,18 @@ func main() {
 	cronManager.Start()
 	defer cronManager.Stop()
 
-	// 启动服务
+	// // 启动服务
+	// httpport, _ := beego.AppConfig.DIY("httpport")
+	// beego.Run(":" + strconv.Itoa(httpport.(int)))
+	// fmt.Println("---->>>---")
+
 	httpport, _ := beego.AppConfig.DIY("httpport")
-	beego.Run(":" + strconv.Itoa(httpport.(int)))
-	fmt.Println("---->>>---")
+	port := strconv.Itoa(httpport.(int))
+	err := grace.ListenAndServe(":"+port, beego.BeeApp.Handlers)
+	if err != nil {
+		global.Log.Error("Server on %v stopped, err: %v", port, err)
+	} else {
+		global.Log.Info("Server on %v stopped", port)
+	}
+	os.Exit(0)
 }
